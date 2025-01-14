0f0fca31bf3c0586f54dbd44bfb45cf05210328c
ᗜ ‸ ᗜ <57232813+iusx@users.noreply.github.com>
2025-01-15 02:14:29 +0800

    feat: add scroll-behavior: smooth;

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -9,8 +9,13 @@ p a {
   color: rgb(152, 152, 152);
   text-decoration: none;
   transition: color 0.3s ease;
+
   &:hover {
     color: blue;
   }
+}
+
+html {
+  scroll-behavior: smooth;
 }
 </style>
