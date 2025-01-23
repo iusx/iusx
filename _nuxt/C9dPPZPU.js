b564b9ca2ac652c83877308ce920c3ceed1ecc2c
iusx <iusx@users.noreply.github.com>
2025-01-23 15:23:01 +0000

    deploy: 6ee906dbdbbffa1533ecb0628974d406e8ebabc6

--- _nuxt/C9dPPZPU.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./BV0Np6rU.js";import"./B2HHJydj.js";import"./Cgb0lF4l.js";import"./BsYmvPZw.js";import"./DC8DfHIg.js";export{o as default};
---
+++ _nuxt/CDQCmeL_.js
@@ -0,0 +1,1 @@
+import{u as s}from"./D5jxm5B7.js";import{b as u}from"./BKLcBA5K.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
