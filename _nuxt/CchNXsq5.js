f34e3827fc0469c7dfad636f22a42d1f6e8fe2de
iusx <iusx@users.noreply.github.com>
2025-01-23 14:21:01 +0000

    deploy: 7f2cff1aff5d4983291c9abfaa4c4295481b9cd8

---
+++ _nuxt/CchNXsq5.js
@@ -0,0 +1,1 @@
+import{u as r}from"./B9lWq7JI.js";import{o as e,q as s,B as o}from"./Cgb0lF4l.js";const i={key:0,class:"text-tip big"},l={key:1,class:"text-tip"},m={__name:"text-tip",props:["type"],setup(a){return r(),(t,n)=>a.type=="big"?(e(),s("main",i,[o(t.$slots,"default")])):(e(),s("main",l,[o(t.$slots,"default")]))}};export{m as default};
--- _nuxt/Jo6P-JNG.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DDQ1xGCp.js";import"./BWHEVbsf.js";import"./Cgb0lF4l.js";import"./BsYmvPZw.js";import"./BH5RN1TX.js";export{o as default};
