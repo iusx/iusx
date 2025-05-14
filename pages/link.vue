278202149cf7946f89e3bc1d5e59e6ee65612afa
ദ്ദി˶𖥦)🍋 <rhyme.qing@gmail.com>
2025-05-15 00:01:19 +0800

    fix: link page phone layout error

--- pages/link.vue
+++ pages/link.vue
@@ -53,18 +53,18 @@ onMounted(() => {
 <template>
   <main>
     <div class="title">
-      <a href="/"
-        ><Icon name="BackIcon" size="2.2vh" />
-        <p>友情链接</p></a
-      >
+      <a href="/">
+        <Icon name="BackIcon" size="20px" />
+        <p>友情链接</p>
+      </a>
     </div>
     <div class="main-layout">
       <div class="main-box" v-for="link in sortedData">
         <a :href="link.url" target="_blank">
           <div v-if="loading" class="no-img"></div>
           <img v-else :src="link.img" :alt="link.name" />
-          <div class="main-box-title">{{ link.name }}</div>
         </a>
+        <div class="main-box-title">{{ link.name }}</div>
       </div>
     </div>
   </main>
@@ -72,10 +72,12 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 @keyframes loading {
+
   0%,
   100% {
     opacity: 1;
   }
+
   50% {
     opacity: 0.5;
   }
@@ -97,16 +99,20 @@ onMounted(() => {
 .main-layout {
   margin-top: 2vw;
 }
+
 .dark-mode .main-box {
   background: #ffffff17;
   box-shadow: 0px 4px 64.5px 11px rgb(255 255 255 / 12%);
 }
+
 .dark-mode .main-box img {
   filter: brightness(0.5);
 }
+
 .main-box a {
   text-decoration: none;
 }
+
 .main-box {
   background: transparentize;
   padding: 1vw;
@@ -116,48 +122,60 @@ onMounted(() => {
     filter 01.3s ease,
     opacity 0.3s ease;
   margin-bottom: 5vw;
+
   img {
     width: 100%;
     border-right: 3px;
   }
+
   .main-box-title {
-    margin-top: 0.8vw;
-    font-size: 1.2vw;
+    margin-top: 12px;
+    font-size: 22px;
     color: #424242;
+
+
     @media (max-width: 1440px) {
-      font-size: 2vw;
-      margin-top: 2vw;
-      margin-bottom: 2vw;
+      font-size: 16px;
+      padding: 4px;
     }
   }
+
   &:hover {
     opacity: 0.5;
   }
 }
+
 .title a {
   display: flex;
   align-items: center;
   text-decoration: none;
   color: #000;
+  margin-bottom: 20px;
+
   p {
     padding: a;
     margin: 0;
     font-weight: bold;
-    font-size: 1.1vw;
+    font-size: 20px;
+
     @media (max-width: 1440px) {
       font-size: 2vw;
     }
   }
 }
+
 .dark-mode a {
   color: #8b8b8b;
 }
+
 main {
   width: 25%;
   margin: 0 auto;
+
   @media (max-width: 900px) {
     width: 60% !important;
   }
+
   margin-top: 10%;
 }
 </style>
