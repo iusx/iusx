bb54b5bf0d645371c47f4ce6dd8a1cf9cb1005eb
ദ്ദി˶𖥦)ദ്ദി <rhyme.qing@gmail.com>
2025-08-11 02:31:19 +0800

    test: mp4 12

--- pages/test/mp4.vue
+++ pages/test/mp4.vue
@@ -1,70 +1,70 @@
+
 <template>
   <div class="test">
-    <h1>Mp4 test 11</h1>
-    <video id="video" muted playsinline webkit-playsinline autoplay></video>
-    <pre>{{ playerInfo }}</pre>
+    <p>Mp4 test 11</p>
+    <div v-if="!started" class="poster" @click="startVideo">
+      <img src="/test/poster.jpg" alt="poster" />
+      <div class="play-btn">▶</div>
+    </div>
+    <video
+      v-show="started"
+      id="video"
+      muted
+      playsinline
+      webkit-playsinline
+      loop
+    ></video>
   </div>
 </template>
 
 <script setup>
 import Hls from "hls.js";
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 
-const playerInfo = ref({});
+const started = ref(false);
 
-function detectPlayerSupport() {
-  const video = document.createElement("video");
-
-  const support = {
-    userAgent: navigator.userAgent,
-    hlsNative: video.canPlayType("application/vnd.apple.mpegurl") !== "",
-    mp4: video.canPlayType("video/mp4") !== "",
-    webm: video.canPlayType("video/webm") !== "",
-    ogg: video.canPlayType("video/ogg") !== "",
-    hlsJsSupport: Hls.isSupported(),
-    autoplayInline: false,
-    playsinline: "playsInline" in video,
-    mutedAutoplay: false,
-  };
-
-  video.muted = true;
-  const playPromise = video.play();
-  if (playPromise !== undefined) {
-    playPromise
-      .then(() => {
-        support.autoplayInline = true;
-        support.mutedAutoplay = true;
-      })
-      .catch(() => {
-        support.autoplayInline = false;
-        support.mutedAutoplay = false;
-      });
-  }
-
-  return support;
-}
-
-onMounted(() => {
+function startVideo() {
+  started.value = true;
   const video = document.getElementById("video");
-  const hls = new Hls();
-  hls.loadSource("/test/playlist.m3u8");
-  hls.attachMedia(video);
 
-  playerInfo.value = detectPlayerSupport();
-});
+  if (Hls.isSupported()) {
+    const hls = new Hls();
+    hls.loadSource("/test/playlist.m3u8");
+    hls.attachMedia(video);
+    video.play();
+  } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+    video.src = "/test/playlist.m3u8";
+    video.play();
+  }
+}
 </script>
 
 <style lang="scss">
 .test {
   height: 300vh;
   width: 100%;
+
+  .poster {
+    position: relative;
+    width: 100%;
+    img {
+      width: 100%;
+      display: block;
+    }
+    .play-btn {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      font-size: 40px;
+      color: white;
+      background: rgba(0, 0, 0, 0.5);
+      border-radius: 50%;
+      padding: 10px;
+    }
+  }
+
   video {
-    &::-webkit-media-controls {
-      display: none !important;
-    }
-    &::-webkit-media-controls-enclosure {
-      display: none !important;
-    }
     width: 100%;
   }
 }
