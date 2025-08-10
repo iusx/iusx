8532042c863d899646c539de4de03925b112206f
ദ്ദി˶𖥦)ദ്ദി <rhyme.qing@gmail.com>
2025-08-11 00:01:16 +0800

    test: mp4 3

--- pages/test/mp4.vue
+++ pages/test/mp4.vue
@@ -1,32 +1,58 @@
 <template>
+  <p>Mp4 test 3</p>
   <div class="test">
-    <p>Mp4 test 2</p>
-    <video
-      class="video"
-      muted
-      playsinline
-      webkit-playsinline
-      x5-video-player-type="h5"
-      preload="none"
-      crossorigin="anonymous"
-      data-src="/test/test.mp4"  
-      aria-label="动画演示..."
-    ></video>
+    <video muted="" playsinline="" src="/test/test.mp4"></video>
   </div>
 </template>
 
-<script setup>
-import { onMounted } from 'vue';
+<script setup></script>
 
-onMounted(() => {
-  const video = document.querySelector('.video');
-  if(video) {
+<style lang="scss">
+.test {
+  height: 200vh;
+  width: 100%;
+  video {
+    width: 100%;
 
-    setTimeout(() => {
-      video.src = video.getAttribute('data-src');
-    }, 500);
+    -webkit-media-controls {
+      display: none !important;
+    }
 
-    video.load();
+    -webkit-media-controls-play-button {
+      display: none !important;
+    }
+
+    -webkit-media-controls-timeline {
+      display: none !important;
+    }
+
+    -webkit-media-controls-mute-button,
+    -webkit-media-controls-volume-slider {
+      display: none !important;
+    }
+
+    -webkit-media-controls-fullscreen-button {
+      display: none !important;
+    }
+
+    -webkit-media-controls-current-time-display,
+    -webkit-media-controls-time-remaining-display {
+      display: none !important;
+    }
+
+    -webkit-media-controls-toggle-closed-captions-button {
+      display: none !important;
+    }
+
+    &::-webkit-media-controls-panel {
+      display: none !important;
+    }
+
+    &::-webkit-media-controls-enclosure {
+      display: none !important;
+    }
+
+    pointer-events: none;
   }
-});
-</script>
+}
+</style>
