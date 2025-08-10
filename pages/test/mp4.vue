6c559d1bbe1c6519b99c057028a1929231af3a85
ദ്ദി˶𖥦)ദ്ദി <rhyme.qing@gmail.com>
2025-08-11 02:02:01 +0800

    test: mp4 9

--- pages/test/mp4.vue
+++ pages/test/mp4.vue
@@ -2,19 +2,19 @@
   <div class="test">
     <p>Mp4 test</p>
 
-    <video id="video" muted="" playsinline=""></video>
+    <video id="video" muted playsinline webkit-playsinline autoplay></video>
   </div>
 </template>
 
 <script setup>
-import Hls from 'hls.js'
+import Hls from "hls.js";
 
 onMounted(() => {
-  const video = document.getElementById('video')
-  const hls = new Hls()
-  hls.loadSource('/test/playlist.m3u8')
-  hls.attachMedia(video)
-})
+  const video = document.getElementById("video");
+  const hls = new Hls();
+  hls.loadSource("/test/playlist.m3u8");
+  hls.attachMedia(video);
+});
 </script>
 
 <style lang="scss">
@@ -22,6 +22,12 @@ onMounted(() => {
   height: 300vh;
   width: 100%;
   video {
+    &::-webkit-media-controls {
+      display: none !important;
+    }
+    &::-webkit-media-controls-enclosure {
+      display: none !important;
+    }
     width: 100%;
   }
 }
