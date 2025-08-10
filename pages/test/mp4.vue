8d6bc2462af14de3c5bc45f6f75fe391c8579bb1
ദ്ദി˶𖥦)ദ്ദി <rhyme.qing@gmail.com>
2025-08-11 02:21:13 +0800

    test: mp4 11

--- pages/test/mp4.vue
+++ pages/test/mp4.vue
@@ -1,35 +1,56 @@
 <template>
   <div class="test">
-    <p>Mp4 test</p>
-    <video
-      id="video"
-      muted
-      autoplay
-      playsinline
-      webkit-playsinline
-      loop
-    ></video>
+    <h1>Mp4 test 11</h1>
+    <video id="video" muted playsinline webkit-playsinline autoplay></video>
+    <pre>{{ playerInfo }}</pre>
   </div>
 </template>
 
 <script setup>
 import Hls from "hls.js";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+
+const playerInfo = ref({});
+
+function detectPlayerSupport() {
+  const video = document.createElement("video");
+
+  const support = {
+    userAgent: navigator.userAgent,
+    hlsNative: video.canPlayType("application/vnd.apple.mpegurl") !== "",
+    mp4: video.canPlayType("video/mp4") !== "",
+    webm: video.canPlayType("video/webm") !== "",
+    ogg: video.canPlayType("video/ogg") !== "",
+    hlsJsSupport: Hls.isSupported(),
+    autoplayInline: false,
+    playsinline: "playsInline" in video,
+    mutedAutoplay: false,
+  };
+
+  video.muted = true;
+  const playPromise = video.play();
+  if (playPromise !== undefined) {
+    playPromise
+      .then(() => {
+        support.autoplayInline = true;
+        support.mutedAutoplay = true;
+      })
+      .catch(() => {
+        support.autoplayInline = false;
+        support.mutedAutoplay = false;
+      });
+  }
+
+  return support;
+}
 
 onMounted(() => {
   const video = document.getElementById("video");
-  const token = btoa(Date.now() + "secret_key");
-  const url = `/test/playlist.m3u8?token=${token}`;
+  const hls = new Hls();
+  hls.loadSource("/test/playlist.m3u8");
+  hls.attachMedia(video);
 
-  if (Hls.isSupported()) {
-    const hls = new Hls();
-    hls.loadSource(url);
-    hls.attachMedia(video);
-    video.play().catch(err => console.warn("Autoplay blocked:", err));
-  } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-    video.src = url;
-    video.play().catch(err => console.warn("Autoplay blocked:", err));
-  }
+  playerInfo.value = detectPlayerSupport();
 });
 </script>
 
@@ -38,13 +59,13 @@ onMounted(() => {
   height: 300vh;
   width: 100%;
   video {
-    width: 100%;
     &::-webkit-media-controls {
       display: none !important;
     }
     &::-webkit-media-controls-enclosure {
       display: none !important;
     }
+    width: 100%;
   }
 }
 </style>
