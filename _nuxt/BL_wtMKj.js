079365eabfb7be603290054fd77c5cb98ef52c6f
iusx <iusx@users.noreply.github.com>
2025-08-20 10:21:24 +0000

    deploy: 91d86b332aa63fef54055c488f319ca3da7f83b5

--- _nuxt/BL_wtMKj.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DIw-05zE.js";import{z as r,A as t,F as c}from"./Dh2Oq3Os.js";const s={};function n(e,l){return r(),t("blockquote",null,[c(e.$slots,"default")])}const _=o(s,[["render",n]]);export{_ as default};
