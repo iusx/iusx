e24e525c0d6dc5c44e41b64b46bcd2adff5d9df8
˶𖥦 <rhyme.qing@gmail.com>
2026-02-19 04:56:51 +0000

    feat: all page use line style

--- app.vue
+++ app.vue
@@ -1,24 +1,22 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
-import Lenis from "lenis"; 
-import 'lenis/dist/lenis.css';
+import Lenis from "lenis";
+import "lenis/dist/lenis.css";
 
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 
 useHead({
   titleTemplate: (titleChunk) => {
-    return titleChunk ? `${titleChunk} - ritsu` : 'ritsu';
-  }
-})
+    return titleChunk ? `${titleChunk} - ritsu` : "ritsu";
+  },
+});
 
 onMounted(() => {
-
   const lenis = new Lenis({
-
-    smooth: true,   
-    lerp: 0.1,      
-    duration: 1.2,  
-    autoRaf: false, 
+    smooth: true,
+    lerp: 0.1,
+    duration: 1.2,
+    autoRaf: false,
     allowNestedScroll: true,
   });
 
@@ -32,6 +30,8 @@ onMounted(() => {
 
 <template>
   <div id="app">
+    <div class="scanline"></div>
+
     <an-more />
     <NuxtLayout>
       <NuxtPage />
@@ -44,7 +44,9 @@ onMounted(() => {
 
 @font-face {
   font-family: "Tsing";
-  src: url("/Tsing.woff2") format("woff2"), url("/Tsing.woff") format("woff"),
+  src:
+    url("/Tsing.woff2") format("woff2"),
+    url("/Tsing.woff") format("woff"),
     url("/Tsing.ttf") format("truetype");
 }
 
@@ -64,5 +66,42 @@ onMounted(() => {
   opacity: 0;
   visibility: hidden;
   animation: fadeIn 2s 1s forwards;
+}
+
+.scanline {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background:
+    linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%),
+    linear-gradient(
+      90deg,
+      rgba(255, 0, 0, 0.03),
+      rgba(0, 255, 0, 0.01),
+      rgba(0, 0, 255, 0.03)
+    );
+  background-size:
+    100% 4px,
+    3px 100%;
+  pointer-events: none;
+  z-index: 100;
+}
+
+@media (prefers-color-scheme: light) {
+  body,
+  html {
+    background: #f4f5f7;
+  }
+  .scanline {
+    opacity: 0.3;
+    background:
+      linear-gradient(rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.05) 50%),
+      linear-gradient(90deg, rgba(0, 0, 0, 0.02), rgba(0, 0, 0, 0.02));
+    background-size:
+      100% 3px,
+      3px 100%;
+  }
 }
 </style>
